b7134fd7a888d898904cc40f506e6602f431178c
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-03-19 22:53:43 +0300

    added the first book genre

---
+++ project.py
@@ -0,0 +1,24 @@
+print("Hello Adala")
+name=(input("enter your name: "))
+
+
+print (f" Hello {name}, please select your prefered book genre:")
+print("1. TRUE CRIME")
+print("2. MYSTERY")
+print("3. HISTORY")
+print("4. POETRY")
+print("5. FAIRY TALE")
+print("6. WOMEN'FICTION")
+print("7. HORROR")
+print("8. ROMANTIC")
+print("9. CONTEMPORARY LITERATURE")
+print("10. SELF-HELP BOOK")
+
+genre=input()
+
+if genre == "1":
+    print(" 1. In Cold Blood: Four members of the Clutter family, who owned a farm in Kansas, were shot to death in their home.")
+    print(" 2. My Dark Places:When James Ellroy was 10 years old, his mother was murdered." )
+    print(" 3. The Stranger Beside Me: In the 1970s, aspiring crime novelist Ann Rule worked at a suicide hotline with Ted Bundy " )
+    print(" 4. Midnight in Mexico: Alfredo Corchado spent his life building a career as a reporter. " )
+    print(" 5. Killers of the Flower Moon: The richest people per capita in the world were members of the Osage Nation." )
