5ce2c3da5e4ee389a3d89f9ce5cdd0977e854440
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-03-20 21:23:35 +0300

    Added five books to all genre

--- project.py
+++ project.py
@@ -1,18 +1,13 @@
-print("Hello Adala")
-name=(input("enter your name: "))
+name=(input("Enter your name:"))
+print(f"Hello {name}, Welcome to Unguka Book Library")
+print("Please select your preferred book genre:")
 
 
-print (f" Hello {name}, please select your prefered book genre:")
 print("1. TRUE CRIME")
-print("2. MYSTERY")
-print("3. HISTORY")
-print("4. POETRY")
+print("2. WOMEN'FICTION")
+print("3. ROMANTIC")
+print("4. SELF-HELP BOOK")       
 print("5. FAIRY TALE")
-print("6. WOMEN'FICTION")
-print("7. HORROR")
-print("8. ROMANTIC")
-print("9. CONTEMPORARY LITERATURE")
-print("10. SELF-HELP BOOK")
 
 genre=input()
 
@@ -22,3 +17,33 @@ if genre == "1":
     print(" 3. The Stranger Beside Me: In the 1970s, aspiring crime novelist Ann Rule worked at a suicide hotline with Ted Bundy " )
     print(" 4. Midnight in Mexico: Alfredo Corchado spent his life building a career as a reporter. " )
     print(" 5. Killers of the Flower Moon: The richest people per capita in the world were members of the Osage Nation." )
+
+elif genre == "2":
+    print("1. Between you and Us: A grieving woman must choose between the husband she loves and the daughter she lost")
+    print("2. The ball at versailles: Captivating tale of four young women and one life-changing night.")
+    print("3. Happily Never After: When Sophie finds out right before getting married that she has been cheated on (again!)")
+    print("4. The Hidden life of Cecily Larson : A family secret, a DNA test, a journey as rich and early-day circus itself.")
+    print("5. How to be good: Wwhen David stops being the angriest man and begins to be good with the help of his spiritual healer.")
+
+elif genre == "3":
+    print("1. Seven days in June: Seven days to fall in love, fifteen years to forget, and seven days to get it all back again")
+    print("2. Me before you: Louisa Clark takes a job as a caregiver for a wealthy banker who has become paralyzed ")
+    print("3. Heartless: Working as a nanny for a single dad should have been simple. Except I can't keep my eyes off him.")
+    print("4. Eclipse:It continues the story of Bella Swan and her vampire love, Edward Cullen.")
+    print("5. Icebreaker: An ice skater and the hockey captain she hates as they're forced to share a rink.")
+
+elif genre =="4":
+    print("1. The 48 laws of Power: Show you what power looks like, how you can get it, what to do to defend yourself ")
+    print("2. Atomic Habits: Guide to breaking bad behaviors and adopting good ones in four steps.")
+    print("3. The 5AM club: Morning routine that has helped his clients maximize their productivity, activate their best health")
+    print("4. Outliers;  Gladwell examines the factors that contribute to high levels of success.")
+    print('5. Girl, wash your face: nspires women to take their lives into their own hands and make their dreams happen')
+
+elif genre =="5":
+    print("1. The tortoise and the hare: The account of a race between unequal partners has attracted conflicting interpretations")
+    print("2. Scarlet: When Scarlet encounters Wolf, a street fighter who may have information as to her")
+    print("3. The princess and the pea: a princess who is tested to become wife to a lonely prince.")
+    print("4. 12 dancing princesses: A king suspects that his twelve daughters go dancing every night and gave a challenge to a prince.")
+    print("5. The snow queen: The story centers on the struggle between good and evil")
+
+
